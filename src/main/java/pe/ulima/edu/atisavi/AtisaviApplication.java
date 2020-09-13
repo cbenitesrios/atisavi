488f5909efa92e6d27f58d57f440cf5cdc7268d1
carlos benites <cbenites@everis.com>
2020-09-13 18:11:25 -0500

    init

---
+++ src/main/java/pe/ulima/edu/atisavi/AtisaviApplication.java
@@ -0,0 +1,13 @@
+package pe.ulima.edu.atisavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtisaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtisaviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/ulima/edu/atisavi/AtisaviApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.ulima.edu.atisavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtisaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
